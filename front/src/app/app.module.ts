d79a3ef20c97fe83c76db523313eda2c15cc0d09
shastaxc <shastaxc@gmail.com>
2022-04-30 12:06:03 -0400

    Rearrange file structure

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { SharedModule } from './shared/shared.module';
+import { SharedModule } from './shared/common/shared.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
