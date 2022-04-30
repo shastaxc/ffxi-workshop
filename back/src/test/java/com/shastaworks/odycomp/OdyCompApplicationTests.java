3d00738c809ebec6b83a3c76f6d3f13e0bc72ffa
shastaxc <shastaxc@gmail.com>
2022-04-30 00:31:56 -0400

    Init backend

---
+++ back/src/test/java/com/shastaworks/odycomp/OdyCompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shastaworks.odycomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdyCompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
