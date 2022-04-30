ce84561ddb2689b16b6fa04ef8af6fb32d3f8ffc
shastaxc <shastaxc@gmail.com>
2022-04-30 00:31:35 -0400

    Add material imports

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
     declarations: [
@@ -14,9 +14,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        MatToolbarModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        SharedModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
---
+++ front/src/app/shared/mat-components.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const materialModules = [
+    MatIconModule,
+    MatToolbarModule,
+];
+
+@NgModule({
+    imports: [
+        ...materialModules,
+    ],
+    exports: [
+        ...materialModules,
+    ],
+})
+
+export class MaterialComponentsModule { }
